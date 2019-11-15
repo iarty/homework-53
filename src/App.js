19a2173aa015ae45c2073b2ef7ad0a0ca0c2eb58
Barbonov Artem <abarbonov@gmail.com>
2019-11-15 11:45:10 +0600

    completed project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from 'react';
+import TodoItem from './components/ToDoCard/todo';
+import nanoid from 'nanoid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    text: '',
+    todoItems: [
+      { id: nanoid(), value: 'test', isCompleted: false },
+      { id: nanoid(), value: 'test', isCompleted: false },
+      { id: nanoid(), value: 'test', isCompleted: false },
+    ]
+  }
+
+  onChanged = (event) => {
+    const text = event.target.value;
+    this.setState({ text });
+  }
+
+  onDelete = (id) => {
+    console.log(id)
+    const index = this.state.todoItems.findIndex(p => p.id === id)
+    const todoItems = [...this.state.todoItems];
+    todoItems.splice(index, 1);
+    this.setState({ todoItems });
+  }
+
+  onCompleted = (id) => {
+    const index = this.state.todoItems.findIndex(p => p.id === id);
+    const todoItems = [...this.state.todoItems];
+    todoItems[index].isCompleted = !todoItems[index].isCompleted;
+    this.setState({ todoItems });
+  }
+
+  addTodoItems = () => {
+    const newTodo = { id: nanoid(), value: this.state.text, isCompleted: false };
+    const todoItems = this.state.todoItems;
+    todoItems.push(newTodo);
+    this.setState({ todoItems });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="container mt-3 d-flex flex-column align-items-center">
+          <div className="border w-50 text-center p-3 border-primary rounded">
+            <h1 className="text-center">ToDo List</h1>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Todo task" aria-label="Recipient's username" aria-describedby="button-addon2" onChange={this.onChanged} />
+              <div className="input-group-append">
+                <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={this.addTodoItems}>Button</button>
+              </div>
+            </div>
+          </div>
+          <div className="list-group">
+            {this.state.todoItems.map((item) => {
+              return (<TodoItem completed={item.isCompleted} key={item.id} value={item.value} onDelete={() => this.onDelete(item.id)} onCompleted={() => this.onCompleted(item.id)} />)
+            })}
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
-
-export default App;
--- src/components/ToDoCard/todo.js
+++ src/components/ToDoCard/todo.js
@@ -1,11 +1,19 @@
-import React, { Component, Fragment } from 'react'
+import React, { } from 'react'
 
-export default class todo extends Component {
-  render() {
-    return (
-      <Fragment>
-        
-      </Fragment>
-    )
-  }
+export default props => {
+  let classes = ['list-group-item'];
+  props.completed ? classes.push('list-group-item-success') : classes.push('list-group-item-secondary')
+  return (
+    <div className="mt-2" style={{ width: "555px", textDecoration: 'none' }}>
+      <div className={classes.join(' ')}>
+        <div className="d-flex w-100 justify-content-between">
+          <p className="mb-1">{props.value}</p>
+          <div className="text-right">
+            <a style={{ color: "#fff" }} className="mr-2" href="/#" onClick={props.onCompleted}><i className="fas fa-check"></i></a>
+            <a style={{ color: "#fff" }} href="/#" onClick={props.onDelete}><i className="fas fa-trash-alt" href="/#"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
